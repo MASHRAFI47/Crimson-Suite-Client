6a4649908651052434a50d71a6ca7cb0c035fdcf
MASHRAFI47 <mbngms@gmail.com>
2024-05-13 03:39:27 +0600

    newsletter added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Banner/Banner"
 import MapLocation from "./MapLocation/MapLocation"
+import Newsletter from "./Newsletter/Newsletter"
 
 const Home = () => {
   return (
     <div>
         <Banner />
         <MapLocation />
+        <Newsletter />
     </div>
   )
 }
--- src/pages/Home/Newsletter/Newsletter.jsx
+++ src/pages/Home/Newsletter/Newsletter.jsx
@@ -1,7 +1,50 @@
+import Swal from "sweetalert2";
 
 const Newsletter = () => {
+    const handleNewsletter = () => {
+        Swal.fire({
+            title: "Submit your Github username",
+            input: "text",
+            inputAttributes: {
+              autocapitalize: "off"
+            },
+            showCancelButton: true,
+            confirmButtonText: "Look up",
+            showLoaderOnConfirm: true,
+            preConfirm: async (login) => {
+              try {
+                const githubUrl = `
+                  https://api.github.com/users/${login}
+                `;
+                const response = await fetch(githubUrl);
+                if (!response.ok) {
+                  return Swal.showValidationMessage(`
+                    ${JSON.stringify(await response.json())}
+                  `);
+                }
+                return response.json();
+              } catch (error) {
+                Swal.showValidationMessage(`
+                  Request failed: ${error}
+                `);
+              }
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: `${result.value.login}'s avatar`,
+                imageUrl: result.value.avatar_url
+              });
+            }
+          });
+    }
   return (
-    <div>Newsletter</div>
+    <section>
+        <div className="bg-orange-400 h-[15rem] flex justify-center items-center">
+            <button onClick={handleNewsletter} className="btn btn-primary">Subscribe to Newsletter</button>
+        </div>
+    </section>
   )
 }
 
